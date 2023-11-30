45689c930d8873435af0b73d405fe27b010e130f
ThinkPad <rijanananta09@gmail.com>
2023-11-30 20:30:13 +0700

    first commit

---
+++ PF28/AccountEmail.java
@@ -0,0 +1,17 @@
+package PF28;
+
+public class AccountEmail {
+    protected String emailAddres;
+    protected String name;
+
+    public String getEmailAddres() {
+    }
+    public  String  getName() {
+    }
+
+    public void setEmailAddres(String emailAddres) {
+    }
+
+    public void setName(String name) {
+    }
+}
